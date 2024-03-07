024f965c6fc026e29c0cb8e725b6192ef1706517
Doyun Lee <doyn511@ajou.ac.kr>
2024-03-07 14:00:10 +0900

    feat: 로그인 버튼 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import SignUp from "../public/SignUp.png";
+import "./App.css";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <button className="login-btn">
+        <img src={SignUp} alt="로그인버튼" className="login-img" />
+      </button>
+    </>
+  );
 }
 
 export default App;
